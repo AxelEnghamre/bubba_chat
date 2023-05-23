cf95281a08a7be6458cfab3879334f16e05f6b4c
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-05-23 11:58:34 +0200

    callback

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 
 const Navigation = () => {
   const { data: session } = useSession();
+  console.log(session);
   return (
     <nav className=" fixed inset-0 w-screen h-24 flex gap-3 bg-slate-400 text-white justify-center items-center">
       <Link href="/"> Home </Link>
--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -1,5 +1,5 @@
-import GithubProvider from "next-auth/providers/github";
 import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 const authOptions: NextAuthOptions = {
   session: {
@@ -11,6 +11,17 @@ const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          testData: "testData",
+        },
+      };
+    },
+  },
 };
-
 export default authOptions;
