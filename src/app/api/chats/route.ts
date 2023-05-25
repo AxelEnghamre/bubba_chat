18e885bd695137112113b00361292c982eaa51a6
Axel Enghamre <axel@enghamre.se>
2023-05-25 18:26:53 +0200

    api for fetching chats

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,23 +1,42 @@
 import supabase from "@/lib/supabaseStore";
-import { NextResponse } from "next/server";
 
 const POST = async (request: Request) => {
   const body: { userId: string } = await request.json();
 
-  console.log("API chats");
-  console.log(body);
-
   const userId = body.userId;
 
-  const { data, error } = await supabase
-    .from("chats")
-    .select()
-    .or(`user_one.eq.${userId},user_two.eq.${userId}`);
+  if (userId) {
+    const { data, error } = await supabase
+      .from("chats")
+      .select()
+      .or(`user_one.eq.${userId},user_two.eq.${userId}`);
 
-  console.log(data);
+    if (error) {
+      return new Response(
+        JSON.stringify({ success: false, error: "supabase error" }),
+        {
+          status: 500,
+        }
+      );
+    }
 
-  const j = JSON.stringify({ chats: ["hej"] });
-  return NextResponse.json(j);
+    if (data.length > 0) {
+      return new Response(JSON.stringify({ success: true, chats: data }), {
+        status: 200,
+      });
+    }
+
+    return new Response(JSON.stringify({ success: false, chats: data }), {
+      status: 401,
+    });
+  }
+
+  return new Response(
+    JSON.stringify({ success: false, error: "missing userId" }),
+    {
+      status: 417,
+    }
+  );
 };
 
 export { POST };
